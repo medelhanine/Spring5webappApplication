2a139e12c8df7289b595a910a51d34850dd39866
medelhanine <mohamedelhanine94@gmail.com>
2018-05-13 16:44:05 +0100

    src

---
+++ src/main/java/test/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -0,0 +1,4 @@
+package test.springframework.spring5webapp.bootstrap;
+
+public class DevBootstrap {
+}
---
+++ src/main/java/test/springframework/spring5webapp/model/Author.java
@@ -0,0 +1,82 @@
+package test.springframework.spring5webapp.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+@Entity
+public class Author {
+    @Id  //tell the JPA this the od
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @ManyToMany(mappedBy = "authors") //many to many relationship
+    private Set<Book> books = new HashSet<>();
+
+    public Author(){
+
+    }
+
+    public Author(String firstName, String lastName, Set<Book> books) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id.equals(author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+}
