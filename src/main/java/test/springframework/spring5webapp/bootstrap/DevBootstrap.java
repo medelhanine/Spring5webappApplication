5d1e15896b61a7a4b659798ace071e0a91e0f6af
medelhanine <mohamedelhanine94@gmail.com>
2018-05-13 16:44:19 +0100

    commit all

--- src/main/java/test/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/test/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -1,4 +1,15 @@
 package test.springframework.spring5webapp.bootstrap;
 
-public class DevBootstrap {
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import test.springframework.spring5webapp.model.Author;
+import test.springframework.spring5webapp.model.Book;
+
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private void initData()
+    {
+        Author eric = new Author("Eric","Evans");
+        Book ddd = new Book(title :"the book title",isbn:"1234", publisher : "name of the publisher" );
+    }
 }
